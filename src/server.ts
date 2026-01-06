311af043bae9f8b70bfcb448e654d4ca7559af84
KSRanasinghe <kesharashakthi71@gmail.com>
2026-01-06 07:47:44 +0530

    setup postgres connection pool

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app";
+import { dbPool } from "./config/db";
 
 const PORT = process.env.PORT || 3000;
+
+
+dbPool
+  .query("select 1")
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("DB connection failed", err));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
